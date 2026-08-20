17a084969b151f532d0f83677d52a61b6b69c035
nry422 <henrymuller@gmail.com>
2026-08-20 15:47:56 -0300

    continuacao aula 20 do 08

--- 2026-08-17-Nivelamento03/src/com/ifsc/tds/Area.java
+++ 2026-08-17-Nivelamento03/src/com/ifsc/tds/Area.java
@@ -2,7 +2,7 @@ package com.ifsc.tds;
 
 public class Area {
 	public double fazer(double raio) {
-		double area = 3.14159 * raio * raio;
+		double area = Math.PI * raio * raio;
 		return area;
 	}
 
